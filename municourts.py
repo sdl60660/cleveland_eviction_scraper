940e8934c99f3c41b9c4f0ff17d5760efa576505
sdl60660 <learnersd@gmail.com>
2020-12-17 02:23:45 -0500

    Re-organized/re-factored full crawler code base to make it cleaner/more efficient/more modular. It was a huge mess before and now it's a little better.

--- municourts.py
+++ municourts.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 import requests
 
@@ -10,11 +9,11 @@ from datetime import datetime, timedelta
 import urllib.request
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
@@ -25,27 +24,34 @@ from collections import Counter
 
 START_PAGE = 'https://clevelandmunicipalcourt.org/public-access'
 
-class MuniCourtTracker():
+class MuniCourtCrawler():
 
-    def __init__(self):
-        # cookies = [{'domain': 'eservices.cmcoh.org', 'httpOnly': True, 'name': 'JSESSIONID', 'path': '/eservices', 'secure': True, 'value': '41E51EECE106E6611F15B6B7359DD055'}]                
-        cookies = pickle.load(open("cookies.pkl", "rb"))
+    def __init__(self, output_file, headless=True):
         chrome_options = webdriver.ChromeOptions()
         # chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
+        if headless:
+            chrome_options.add_argument('--headless')
         
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.cookies = pickle.load(open("cookies.pkl", "rb"))
+        self.outfile = output_file
+
+    
+    def __repr__(self):
+        return ('<Selenium Driver for CLE Municipal Courts. Current Page: {}>'.format(str(self.driver.current_url)))
+    
+
+    def enter_site(self):
         self.driver.get(START_PAGE)
-        for cookie in cookies:
+        for cookie in self.cookies:
             self.driver.add_cookie(cookie)
         self.driver.implicitly_wait(8)
 
         # Click "I Accept" button on intial homepage
         try:
             self.click_button_name(button_name="   I Accept   ")
-            time.sleep(2)
         except:
-            time.sleep(6)
+            time.sleep(4)
             self.click_button_name(button_name="   I Accept   ")
 
 
@@ -63,22 +69,27 @@ class MuniCourtTracker():
             time.sleep(1)
 
         except NoSuchElementException:
-            captcha_image = None
+            pass
 
         # Get to search page
-        for button in ["Click Here", "Case Type Search"]:
+        for button in ["Click Here"]:
             try:
                 self.click_button_name(button_name=button)
-                time.sleep(2)
             except:
-                time.sleep(6)
+                time.sleep(4)
                 self.click_button_name(button_name=button)
 
         pickle.dump(self.driver.get_cookies(), open("cookies.pkl","wb"))
         # print(self.driver.get_cookies())
+    
 
-    def __repr__(self):
-        return ('<Selenium Driver for CLE Municipal Courts. Current Page: {}>'.format(str(self.driver.current_url)))
+    def navigate_to_search_menu(self, menu_name):
+        if not self.is_element_on_page('//span[text()="{}"]'.format(menu_name)):
+            raise ValueError("Not a valid search menu name. At the time this was built, the search menu options were: 'Name Search'," + 
+                            "'Ticket/Citation # Search', 'Case Number Search', and 'Case Type Search'. It's possible that these values " + 
+                            "have changed, but regardless, the value you entered is not on the search menu page.")
+
+        self.click_button_name(button_name=menu_name)
 
 
     def grab_captcha_image(self, captcha_image, filename):
@@ -131,12 +142,127 @@ class MuniCourtTracker():
         consensus_answer = answer_counts.most_common(1)[0][0]
         return consensus_answer
 
+    
+    def set_search_options(self):
+        time.sleep(0.5)
+
+        # Hard-coded selection for 'Number of Results'
+        self.click_button_xpath(button_xpath='//*[@name="bodyLayout:topSearchPanel:pageSize"]/option[@value="2"]')
+        # Hard-coded selection for 'Case Type': "CVG - LANDLORD/TENANT" (Pre-select, this needs to be "clicked" twice)
+        self.click_button_xpath(button_xpath='//*[@name="caseCd"]/option[7]')
+
+        # tracker.click_button_xpath(button_xpath='//*[@name="ptyCd"]/option[12]')
+        # tracker.click_button_xpath(button_xpath='//*[@id="id22"]/option[@value="2"]')
+        time.sleep(0.5)
+
+        # Hard-coded selection for 'Case Type': "CVG - LANDLORD/TENANT"
+        self.click_button_xpath(button_xpath='//*[@name="caseCd"]/option[7]')
+
+        # Hard-coded selection for 'Party Type'
+        for x in range(2):
+            self.scroll_to_element(element_xpath='//*[@name="ptyCd"]/option[12]')
+            time.sleep(0.5)
+    
+    
+    def fill_dates_and_press(self, date_string):
+        # Fill Start Date box
+        self.fill_box(element_id=None, element_xpath='//*[@name="fileDateRange:beginDate"]', text=date_string)
+        time.sleep(0.2)
+
+        # Fill End Date box
+        self.fill_box(element_id=None, element_xpath='//*[@name="fileDateRange:endDate"]', text=date_string)
+
+        # Press Submit button
+        self.click_button_xpath(button_xpath='//*[@name="submitLink"]')
+        #tracker.wait_until_loaded('//*[@id="grid"]/tbody/tr//a')
+
+    
+    def scrape_page_results(self, current_page):
+        # Find case elements on the page
+        try:
+            rows = self.get_num_table_rows()
+        except:
+            time.sleep(2)
+            rows = self.get_num_table_rows()
+        
+        rows = min(40, rows - (40 * (current_page - 1)))
+        
+        # For each case element
+        for row_num in range(rows):
+            try:
+                row = self.get_table_row((row_num+1))
+                # Follow case link
+                row.click()
+
+                # Parse/store data
+                csv_dict = self.parse_data()
+                self.store_data(csv_dict)
+
+                # Go back to previous page with other case elements
+                self.back_page()
+            except Exception as e:
+                time.sleep(1)
+                print(e)
+
+
+    def search_date(self, date, current_page_index):
+        # IMPORTANT: Can only run starting from "Case Type Search" menu (will return to this menu at function end)
+        
+        # If not search menu page, throw error and exit
+        if not self.is_element_on_page('//span[text()="Case Type Search"]'):
+            raise RuntimeError("The search_date method can only be run from the search page. Crawler must be navigated to that page before running (using 'enter_site', then 'navigate_to_search_menu')")
+        # If not on the "Case Type Search" tab of the search menu, throw error and exit
+        if 'selected' not in self.driver.find_element_by_xpath('//span[text()="Case Type Search"]/ancestor::li').get_attribute("class"):
+            raise RuntimeError("The search_date method can only be run from the 'Case Type Search' tab. Crawler must be navigated to that tab before running (using 'navigate_to_search_menu)")
+
+        date_string = datetime.strftime(date, '%m/%d/%Y')
+        print(date_string)
+
+        self.set_search_options()
+        self.fill_dates_and_press(date_string)
+        
+        while self.is_element_on_page('//span[@class="feedbackPanelERROR"]'):
+            self.set_search_options()
+            self.fill_dates_and_press(date_string)
+            # errors = self.driver.find_elements_by_xpath('//span[@class="feedbackPanelERROR"]')
+
+        num_pages = self.get_num_results_pages()
+        print(num_pages)
+        print(current_page_index)
+        
+        # Click on the page button for corresponding page number if we're looking for someting other than page 1
+        if current_page_index > 1:
+            self.click_button_xpath('//*[@title="Go to page {}"]'.format(current_page_index))
+        
+        self.scrape_page_results(current_page_index)
+
+        # Try to return to search page and increment date
+        # If the site sends us back to the welcome page, which seems to happen after a lot of quick scraping, re-enter with 'Click Here' button
+        # self.click_button_name(button_name="Search")
+        # if self.is_element_on_page(element_xpath='//span[text()="Click Here"]'):
+        #     self.click_button_name(button_name="Click Here")
+
+        if current_page_index == num_pages:
+            date += timedelta(days=1)
+            current_page_index = 1
+        else:
+            current_page_index += 1
+
+        return date, current_page_index
+
 
     def click_button_name(self, button_name):
         button = self.driver.find_element_by_link_text(button_name).click()
 
+
     def click_button_xpath(self, button_xpath):
         button = self.driver.find_element_by_xpath(button_xpath).click()
+
+
+    def is_element_on_page(self, element_xpath):
+        matching_elements = self.driver.find_elements(By.XPATH, element_xpath)
+        return len(matching_elements) > 0
+
 
     def scroll_to_element(self, element_xpath):
         element = self.driver.find_element_by_xpath(element_xpath)
@@ -144,6 +270,7 @@ class MuniCourtTracker():
         actions.move_to_element(element)
         actions.click()
         actions.perform()
+
 
     def fill_box(self, element_id, element_xpath, text):
         if element_id:
@@ -156,6 +283,25 @@ class MuniCourtTracker():
             raise ValueError('Must specify either an element_id or element_xpath')
         element.send_keys(text)
 
+    
+    def get_num_results_pages(self):
+        # Split the result string, which will either be in the format "Displaying 100 of ___ total matches." if > 100 or "Displaying all ___ matches." if <= 100
+        # Find the last integer in the string, which should be the total number of results from the date
+        try:
+            total_results = [int(x) for x in self.driver.find_element_by_id("srchResultNotice").text.split(" ") if MuniCourtCrawler.is_int(x)][-1]
+            # Round down for now
+            total_results = min(100, total_results)
+        except:
+            total_results = 0
+        
+        # CourtView will display a max of three pages of results and a max of 100 items, so find how many pages we'd expect from the results.
+        # If there are more than 100 results for the selected date, we have a small problem that we can work on later
+        # There are 40 results to a page, so floor divide by 40 (and add one)
+        num_pages = min(3, ((total_results // 41) + 1))
+        
+        return num_pages
+
+
     def get_num_table_rows(self):
         text = self.driver.find_elements_by_xpath('*//div[@id="srchResultNotice"]')
         if len(text) == 0:
@@ -167,35 +313,21 @@ class MuniCourtTracker():
             num_rows = int(text[0].text.split(' ')[2])
             return num_rows
 
+
     def get_table_row(self, num):
         return self.driver.find_elements_by_xpath('//*[@id="grid"]/tbody/tr[{}]//a'.format(num))[0]
 
-    def search_records(self):
-        pass
 
     def parse_to_soup(self):
         html = self.driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
         return soup
 
-    @staticmethod
-    def get_address_info(row):
-        contact_data = row.find('div', attrs={'class': 'box ptyContact'})
-        address = contact_data.find('dl').find('dd')
-        address_line_1 = address.find('div',attrs={'class': 'addrLn1'}).text
-        # address_line_2 = address.find('div',attrs={'class': 'addrLn2'}).text
-        try:
-            city = address.find_all('span')[0].text.title() + ', ' + address.find_all('span')[1].text
-        except:
-            city = 'Cleveland, OH'
 
-        return address_line_1, city
-
-
-    def store_data(self, filename):
+    def parse_data(self):
         soup = self.parse_to_soup()
 
-        csv_dict = {}
+        data_dict = {}
 
         # Case Name
         try:
@@ -204,11 +336,11 @@ class MuniCourtTracker():
             time.sleep(4)
             case_name = self.driver.find_elements_by_xpath('*//div[@id="titleBar"]//h2')[0].text.replace('\t', '').replace('\n', '').strip(' ')
 
-        csv_dict['Case Name'] = case_name
+        data_dict['Case Name'] = case_name
 
         # Case Number
         case_number = ' '.join(case_name.split(' ')[:3])
-        csv_dict['Case Number'] = case_number
+        data_dict['Case Number'] = case_number
 
         # Case Status, File Date, Action
         table = soup.find('table')
@@ -221,7 +353,7 @@ class MuniCourtTracker():
             if len(title) == 1:
                 for field in {'Case Status', 'File Date', 'Action'}:
                     if field in title[0].text:
-                        csv_dict[field] = data[0].text
+                        data_dict[field] = data[0].text
 
         # Address, Defendants, Plaintiff
         defendants = []
@@ -243,61 +375,102 @@ class MuniCourtTracker():
             elif text.split(' - ')[1] == 'PLAINTIFF':
                 plaintiffs.append(text.split(' - ')[0])
                 try:
-                    csv_dict['Plaintiff Address'], csv_dict['Plaintiff City'] = MuniCourtTracker.get_address_info(row)
+                    data_dict['Plaintiff Address'], data_dict['Plaintiff City'] = MuniCourtCrawler.get_address_info(row)
                 except:
-                    csv_dict['Plaintiff Address'], csv_dict['Plaintiff City'] = 'Address Error', 'Address Error'
+                    data_dict['Plaintiff Address'], data_dict['Plaintiff City'] = 'Address Error', 'Address Error'
 
             elif text.split(' - ')[1] == 'PROPERTY ADDRESS':
                 try:
-                    csv_dict['Property Address'], csv_dict['Property City'] = MuniCourtTracker.get_address_info(row)
+                    data_dict['Property Address'], data_dict['Property City'] = MuniCourtCrawler.get_address_info(row)
                 except:
-                    csv_dict['Plaintiff Address'], csv_dict['Plaintiff City'] = 'Address Error', 'Address Error'
+                    data_dict['Plaintiff Address'], data_dict['Plaintiff City'] = 'Address Error', 'Address Error'
 
-        csv_dict['Plaintiff'] = '; '.join(plaintiffs)
-        csv_dict['Defendants'] = '; '.join(defendants)
+        data_dict['Plaintiff'] = '; '.join(plaintiffs)
+        data_dict['Defendants'] = '; '.join(defendants)
 
         # Costs
         costs_table = soup.find('div', attrs={'id':'financialInfo'}).find('table')
         # try:
-        csv_dict['Costs'] = costs_table.find('tfoot').find_all('th',attrs={'class': 'currency'})[0].text
+        data_dict['Costs'] = costs_table.find('tfoot').find_all('th',attrs={'class': 'currency'})[0].text
         # except:
-            # csv_dict['Costs'] = ''
+            # data_dict['Costs'] = ''
 
 
         # Disposition Status, Disposition Date
 
         disposition_table = soup.find('div', attrs={'id':'dispositionInfo'}).find('table').find('tbody')
-        csv_dict['Disposition Status'] = disposition_table.find_all('td')[0].text
-        csv_dict['Disposition Date'] = disposition_table.find_all('td')[-1].text
+        data_dict['Disposition Status'] = disposition_table.find_all('td')[0].text
+        data_dict['Disposition Date'] = disposition_table.find_all('td')[-1].text
 
+        return data_dict
+
+
+    def store_data(self, csv_dict):
         date_string = datetime.today().strftime('%Y%m%d')
+        case_number = csv_dict['Case Number']
 
         try:
+            # Keep record of scrape on specific date (as data will change)
             with open(f'page_source_files/{date_string}/{case_number}.html', 'w') as f:
+                f.write(self.driver.page_source)
+
+            # Add or replace to store of all files
+            with open(f'page_source_files/all_data/{case_number}.html', 'w') as f:
                 f.write(self.driver.page_source)
         except FileNotFoundError:
             os.mkdir(os.getcwd() + '/page_source_files/' + datetime.today().strftime('%Y%m%d'))
             with open(f'page_source_files/{date_string}/{case_number}.html', 'w') as f:
                 f.write(self.driver.page_source)
 
-
-
         # NEW FIELDS HERE
-        MuniCourtTracker.write_to_csv(filename, csv_dict)
+        MuniCourtCrawler.write_to_csv(self.outfile, csv_dict)
+
+
+    def back_page(self):
+        self.driver.back()
+
+
+    def quit(self):
+        self.driver.quit()
+
 
     @staticmethod
     def write_to_csv(filename, dictionary):
+        # If output file doesn't exist yet, create and add header. Otherwise, we're appending to an existing file
+        if os.path.isfile(filename) == False:
+            with open(filename, 'w') as f:
+                fields = ['Case Name', 'Case Number', 'Case Status', 'File Date', 'Action',
+                'Defendants', 'Property Address', 'Property City',
+                'Plaintiff', 'Plaintiff Address', 'Plaintiff City',
+                'Costs', 'Disposition Status', 'Disposition Date']
+                out_csv = csv.DictWriter(f, fieldnames=fields)
+                out_csv.writeheader()
+
         with open(filename, 'a') as f:
             fields = ['Case Name', 'Case Number', 'Case Status', 'File Date', 'Action',
                       'Defendants', 'Property Address', 'Property City',
                       'Plaintiff', 'Plaintiff Address', 'Plaintiff City',
                       'Costs', 'Disposition Status', 'Disposition Date']
-                      # NEW FIELDS HERE
             out_csv = csv.DictWriter(f, fieldnames=fields)
             out_csv.writerow(dictionary)
 
-    def back_page(self):
-        self.driver.back()
+    @staticmethod
+    def get_address_info(row):
+        contact_data = row.find('div', attrs={'class': 'box ptyContact'})
+        address = contact_data.find('dl').find('dd')
+        address_line_1 = address.find('div',attrs={'class': 'addrLn1'}).text
+        # address_line_2 = address.find('div',attrs={'class': 'addrLn2'}).text
+        try:
+            city = address.find_all('span')[0].text.title() + ', ' + address.find_all('span')[1].text
+        except:
+            city = 'Cleveland, OH'
 
-    def quit(self):
-        self.driver.quit()
+        return address_line_1, city
+    
+    @staticmethod
+    def is_int(a):
+        try:
+            return int(a)
+        except ValueError:
+            return False
+
